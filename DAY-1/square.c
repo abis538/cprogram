13e1375a8b5c1498e2d14113ae0cb83331953c99
abis538 <abis53866@gmail.com>
2025-05-16 15:57:19 +0530

    Add files via upload
    DAY-1

---
+++ DAY-1/square.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("Square = %d\n", num * num);
+    printf("Cube = %d\n", num * num * num);
+    return 0;
+}
